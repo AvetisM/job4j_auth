e915c7d81423a0f8fbd0a25f7dd521f6d783638d
AMkhitarants <avetis.mkhitaryants@gmail.com>
2023-04-30 19:02:50 +0300

    1. RESTFul. Описание архитектуры [#6884]

---
+++ src/main/java/ru/job4j/domain/Person.java
@@ -0,0 +1,19 @@
+package ru.job4j.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private int id;
+    private String login;
+    private String password;
+}
