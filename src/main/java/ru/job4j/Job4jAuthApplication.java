ee6f6e28c1cee2e1e1d0d62fee3c7d82310d568c
AMkhitarants <avetis.mkhitaryants@gmail.com>
2023-05-17 10:04:22 +0300

    1. Spring Liquibase [#302330]

--- src/main/java/ru/job4j/Job4jAuthApplication.java
+++ src/main/java/ru/job4j/Job4jAuthApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import liquibase.integration.spring.SpringLiquibase;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Job4jAuthApplication {
@@ -15,5 +17,13 @@ public class Job4jAuthApplication {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SpringLiquibase liquibase(DataSource ds) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+		liquibase.setDataSource(ds);
+		return liquibase;
 	}
 }
